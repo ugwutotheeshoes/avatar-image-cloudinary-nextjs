ac583bc985fceb135d1089f3de82d8783ab45a14
Emmanuel Ugwu <32464178+ugwutotheeshoes@users.noreply.github.com>
2024-06-19 15:07:54 +0100

    new files

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="spinner">
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
